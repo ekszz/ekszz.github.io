5d3ba09e75581adce41a80bfd253f95ae7e2e100
Ekszz <ekszz@users.noreply.github.com>
2023-01-05 00:18:46 +0800

    v2 init

---
+++ README.md
@@ -0,0 +1,7 @@
+# EKSZZ的主页
+
+## 🙆‍♂️ 感谢
+
+[![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=dmego&repo=home.github.io)](https://github.com/dmego/home.github.io)
+
+[![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=lqbby&repo=Tech-Home)](https://github.com/lqbby/Tech-Home)
---
+++ js/main.js
@@ -0,0 +1,60 @@
+var iUp = (function () {
+	var t = 0,
+		d = 150,
+		clean = function () {
+			t = 0;
+		},
+		up = function (e) {
+			setTimeout(function () {
+				$(e).addClass("up")
+			}, t);
+			t += d;
+		},
+		down = function (e) {
+			$(e).removeClass("up");
+		},
+		toggle = function (e) {
+			setTimeout(function () {
+				$(e).toggleClass("up")
+			}, t);
+			t += d;
+		};
+	return {
+		clean: clean,
+		up: up,
+		down: down,
+		toggle: toggle
+	}
+})();
+
+$(document).ready(function () {
+	fetch('https://v1.hitokoto.cn').then(function (res) {
+		return res.json();
+	}).then(function (e) {
+		$('#description').html(e.hitokoto + "<br/> -「<strong>" + e.from + "</strong>」")
+	}).catch(function (err) {
+		console.error(err);
+	})
+
+	$(".iUp").each(function (i, e) {
+		iUp.up(e);
+	});
+
+	$(".js-avatar")[0].onload = function () {
+		$(".js-avatar").addClass("show");
+	}
+});
+
+$('.btn-mobile-menu__icon').click(function () {
+	if ($('.navigation-wrapper').css('display') == "block") {
+		$('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+			$('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+			$('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+		});
+		$('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+
+	} else {
+		$('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+	}
+	$('.btn-mobile-menu__icon').toggleClass('social iconfont icon-list social iconfont icon-ngleup animated fadeIn');
+});
